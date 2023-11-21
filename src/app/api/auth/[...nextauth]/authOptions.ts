2ade219877ebea65372cd6b81b207de7e406c477
Ghorasaini Success <imsaphal21@gmail.com>
2023-11-21 20:03:17 +0545

    setup prisma and nextauth

---
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -0,0 +1,32 @@
+import { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: AuthOptions = {
+  pages: {
+    signIn: "/login",
+  },
+
+  providers: [
+    CredentialsProvider({
+      name: "Sign in with DevUi",
+      credentials: {
+        email: { label: "email", type: "email", placeholder: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
+  ],
+};
